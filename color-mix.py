94086a84835fdddec03b501e6f804e093bfbe19f
twil3 <twil3akine@gmail.com>
2024-08-22 22:04:39 +0900

    adjust Tkinter

--- color-mix.py
+++ color-mix.py
@@ -1,32 +1,46 @@
 import numpy as np
 import cv2 as ct
 import tkinter as tk
-import PIL as pl
+from PIL import Image, ImageTk
 
 class app:
 	def __init__(self):
-		self.img = np.zeros((720,720,3), np.uint8)
-		self.key = None
-		self.height, self.width, self.cannel = self.img.shape
+		self.bgr = np.zeros((180,360,3), np.uint8)
+		self.rgb = np.array([0])
+		self.height, self.width, self.cannel = self.bgr.shape
 
-	def show(self):
-		ct.imshow("Canvas", self.img)
-		
-	def keypress(self):
-		self.key = ct.waitKey(0)
-		if self.key in {ord('q'), 27}:
+		self.root = tk.Tk()
+		self.canvas = tk.Canvas(self.root, width=self.width, height=self.height)
+
+		self.root.title("Canvas")
+		self.root.bind('<Key>', self.keypress)
+		self.canvas.pack()
+
+		self.update()
+
+	def keypress(self, event):
+		key = event.char
+		if key in {'q', chr(27)}:
 			ct.destroyAllWindows()
+			self.root.quit()
 			return True
-		elif self.key in {ord('r'), ord('g'), ord('b')}:
-			self.change()
+		elif key in {'r', 'g', 'b'}:
+			self.change(key)
+			self.update()
 		
-	def change(self):
-		self.img[self.height//4:(self.height//4)*3, self.width//4:(self.width//4)*3] = [150 if self.key != ord('b') else 255, 150 if self.key != ord('g') else 255, 150 if self.key != ord('r') else 255]
+	def change(self, key):
+		self.bgr[0:self.height, 0:self.width] = [150 if key != 'b' else 255, 150 if key != 'g' else 255, 150 if key != 'r' else 255]
+
+	def update(self):
+		self.rgb = ct.cvtColor(self.bgr, ct.COLOR_BGR2RGB)
+		img_pil = Image.fromarray(self.rgb)
+		self.img_tk = ImageTk.PhotoImage(img_pil)
+		self.canvas.create_image(0, 0, image=self.img_tk, anchor="nw")
 
 	def exe(self):
-		while True:
-			if self.keypress() == True: break
-			self.show()
+
+		self.root.mainloop()
+
 
 def main():
 	Application = app()
